c5b51ef04a50eb37f9f5504c9030007e0cb51c98
hrithikweb <81437270+hrithikweb@users.noreply.github.com>
2021-03-26 23:26:22 +0530

    Assignment 1 submitted

---
+++ Assignment 1/one.js
@@ -0,0 +1,11 @@
+var a=document.getElementsByTagName('h1');
+console.log(a[0].innerText);
+
+var a=document.getElementsByClassName('he');
+console.log(a[0].innerText);
+
+var a=document.getElementById('go');
+console.log(a);
+
+var a=document.getElementsByClassName('he');
+a[0].innerText="letsupgrade";
